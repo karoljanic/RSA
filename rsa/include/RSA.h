17884459b3d4d148c17643c53b9dd2bc5c8256ba
karoljanic <karoljanic@gmail.com>
2020-12-06 22:30:05 +0100

    small modify

--- rsa/include/RSA.h
+++ rsa/include/RSA.h
@@ -5,22 +5,22 @@
 class RSA
 {
     public:
-        void generate_keys(); /// generate and save public and private key
-        void encrypt();  /// to do
-        void decrypt();  /// to do
-
+        void generate_keys(); /// generate and save public and private key, Trzeba  wkleic funkcje Micha³a
+        void encrypt(const char *name_of_file);  /// encrypt and save file the name_of_file, new name: name_of_file_encrypted.txt, give the file name without ".txt" Trzeba  wkleic funkcje Micha³a
+        void decrypt(const char *name_of_file,unsigned long long n, unsigned long long d);  /// decrypt and save file the name_of_file,
+                                                                                            /// new name: name_of_file_decrypted.txt, give the file name without ".txt"
+                                                                                            /// private key: (n,d)
     private:
-        unsigned long long public_key[2]; /// [e][n]
-        unsigned long long private_key[2]; /// [d][n]
+        unsigned long long public_key[2]; /// [n][e]
+        unsigned long long private_key[2]; /// [n][d]
         unsigned long long get_rand_number(unsigned long long from, unsigned long long to); /// return random number from interval <from, to>
         unsigned long long expo_mod(unsigned long long a, unsigned long long b, unsigned long long n); /// return a^b % n
         unsigned long long multi_mod(unsigned long long a, unsigned long long b, unsigned long long n); /// return a*b % n
         bool is_prime(unsigned long long number, int num_of_tests); /// return true if number is prime, false otherwise, Miller-Rabin test, probability: 1-(1/4)^num_of_tests
+        bool is_prime_prim(unsigned long long number); /// return true if number is prime, false otherwise, probability: 1
         unsigned long long gcd(unsigned long long a, unsigned long long b); /// return greatest common divisor
         unsigned long long ext_euc_algo(unsigned long long a, unsigned long long b); /// extended Euclidean algorithm, return the modular inverse
         std::string dec_to_hex(unsigned long long x); /// return the number x in hexadecimal
-
-
 
 };
 
--- rsa/src/RSA.cpp
+++ rsa/src/RSA.cpp
@@ -1,8 +1,9 @@
 #include "RSA.h"
+
 #include <random>(std::mt19937_64)
 #include <time.h>
-
 #include <iostream>
+#include <cstdio>
 
 
 unsigned long long RSA::get_rand_number(unsigned long long from, unsigned long long to){
@@ -74,6 +75,16 @@ bool RSA::is_prime(unsigned long long number, int num_of_tests){
   return t;
 }
 
+bool RSA::is_prime_prim(unsigned long long number){
+
+    for(unsigned long long i = 2; i<=sqrt(number); i++){
+        if(number%i == 0)
+            return false;
+    }
+    return true;
+
+}
+
 unsigned long long RSA::gcd(unsigned long long a, unsigned long long b){
 
     unsigned long long t;
@@ -112,25 +123,59 @@ unsigned long long RSA::ext_euc_algo(unsigned long long a, unsigned long long b)
 
 std::string RSA::dec_to_hex(unsigned long long x){
 
+    char hexaDeciNum[16];
+    for(int i=0; i<16; i++)
+        hexaDeciNum[i] = '0';
+    int i = 0;
+    while(x!=0){
+
+        int temp  = 0;
+        temp = x % 16;
+
+        if(temp < 10){
+            hexaDeciNum[i] = temp + 48;
+            i++;
+        }
+        else{
+            hexaDeciNum[i] = temp + 55;
+            i++;
+        }
+
+        x = x/16;
+    }
+
+    std::string res="";
+    for(int i = 0; i<16; i+=2){
+        res+= hexaDeciNum[i+1];
+        res+= hexaDeciNum[i];
+    }
+
+    return res;
 }
 
 void RSA::generate_keys(){
     std::cout << "Generating the key...\nWait! Finding BIG prime numbers is not easy...\n";
 
     unsigned long long p = get_rand_number(65536, 4294967295);
-    while(!is_prime(p,20))
-        p = get_rand_number(65536, 4294967295);
+    //unsigned long long p = 21932789;
+    while(!is_prime_prim(p)){
+        while(!is_prime(p,20))
+            p = get_rand_number(65536, 4294967295);
+    }
     std::cout << "p = " << p << std::endl;
 
     unsigned long long q = get_rand_number(65536, 4294967295);
-    while(!is_prime(q,20) || q==p)
-        q = get_rand_number(65536, 4294967295);
+    //unsigned long long q = 2212563877;
+    while(!is_prime_prim(q) || q==p){
+        while(!is_prime(q,20) || q==p)
+            q = get_rand_number(65536, 4294967295);
+    }
     std::cout << "q = " << q << std::endl;
 
     unsigned long long totient = (p-1)*(q-1);
     unsigned long long n = p*q;
 
-    unsigned long long e = 3;
+    unsigned long long e = 101;
     while(gcd(e,totient)!=1)
         e+=2;
     std::cout << "e = " << e << std::endl;
@@ -138,14 +183,54 @@ void RSA::generate_keys(){
     unsigned long long d = ext_euc_algo(e,totient);
     std::cout << "d = " << d << std::endl;
 
-    std::cout << "klucz publiczny = (" << e << ", " << n << ")" << std::endl;
-    std::cout << "klucz prywatny = (" << d << ", " << n << ")" << std::endl;
+    std::cout << "klucz publiczny = (" << n << ", " << e << ")" << std::endl;
+    std::cout << "klucz prywatny = (" << n << ", " << d << ")" << std::endl;
 
+    private_key[0] = n;
+    private_key[1] = d;
+    public_key[0] = n;
+    public_key[1] = e;
 
-    private_key[0] = d;
-    private_key[1] = n;
-    public_key[0] = e;
-    public_key[1] = n;
+    FILE *priv = fopen("private_key.txt", "w");
+    FILE *publ = fopen("public_key.txt", "w");
+
+    char pub_mag_num[] = {'P','U','B','0'};
+    char priv_mg_num[] = {'P','R','I','V'};
+    fwrite(&pub_mag_num, 1, 4, publ);
+    fwrite(&priv_mg_num, 1, 4, priv);
+
+    fwrite(&n, 8,1,publ);
+    fwrite(&e, 8,1,publ);
+
+    fwrite(&n, 8,1,priv);
+    fwrite(&d, 8,1,priv);
+
+    fclose(priv);
+    fclose(publ);
+
+}
+
+void RSA::encrypt(const char *name_of_file){
+
+    std::string name1 = name_of_file;
+    std::string name2 = name_of_file;
+    name1+=".txt";
+    name2+="_encrypted.txt";
+
+    FILE * file_in = fopen(name1.c_str(),"r");
+    FILE * file_out = fopen(name2.c_str(),"w");
+
+}
+
+void RSA::decrypt(const char *name_of_file, unsigned long long n, unsigned long long d){
+
+    std::string name1 = name_of_file;
+    std::string name2 = name_of_file;
+    name1+=".txt";
+    name2+="_decrypted.txt";
+    FILE * file_in = fopen(name1.c_str(),"r");
+    FILE * file_out = fopen(name2.c_str(),"w");
+
 
 
 }
